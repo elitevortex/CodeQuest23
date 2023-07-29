2ef9290067ca6c2c5a41b6c43fd91dc522ff44ff
88potatoes <94770511+88potatoes@users.noreply.github.com>
2023-07-29 18:51:59 +1000

    latest version

--- cookie_cutter/src/game.py
+++ cookie_cutter/src/game.py
@@ -111,11 +111,11 @@ class Game:
         self.power_ups_distances = []
     
         # # Iterate through the updated objects
-        # for updated_object in self.current_turn_message["message"]["updated_objects"]:
-        #     # if the object has no velocity, just position (Walls: 3 & 4, PowerUps: 7, Boundary is type 5)
-        #     if updated_object["type"] == ObjectTypes.POWERUP.value:
-        #         self.update_powerUp_distances(updated_object["position"], updated_object["powerup_type"])
-        #     # if the object has velocity + position (Tank: 1, Bullet: 2, Closing Boundary is type 6)
+        for updated_object in self.current_turn_message["message"]["updated_objects"]:
+            # if the object has no velocity, just position (Walls: 3 & 4, PowerUps: 7, Boundary is type 5)
+            if updated_object["type"] == ObjectTypes.POWERUP.value:
+                self.update_powerUp_distances(updated_object["position"], updated_object["powerup_type"])
+            # if the object has velocity + position (Tank: 1, Bullet: 2, Closing Boundary is type 6)
 
         
     def update_powerUp_distances(self, position: list(), powerup_type: str) -> None:
